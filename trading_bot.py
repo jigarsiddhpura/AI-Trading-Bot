0191c1e5dc08d5fb71fec11c8f0c0a3d3a704dfb
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-01-31 00:42:03 +0530

    feat : position sizing

--- trading_bot.py
+++ trading_bot.py
@@ -20,20 +20,31 @@ ALPACA_CREDS = {
 }
 
 class MLTrader(Strategy):
-    # this method is called only once , when strategy execution starts
-    def initialize(self, symbol: str = "SPY"):
+    # isLifeCycleMethod , this method is called only once , when strategy execution starts
+    def initialize(self, symbol: str = "SPY", cash_at_risk: float = 0.6):
         self.sleeptime = '24H'
         self.symbol = symbol
         self.last_trade = None
+        self.cash_at_risk = cash_at_risk
 
-    # all trading logic here
+    # dynamically adjusting the quantity to buy
+    def position_sizing(self):
+        last_price = self.get_last_price(asset=self.symbol)
+        cash = self.get_cash()
+        quantity = round((cash * self.cash_at_risk)  / last_price , 0)
+        return last_price,cash,quantity
+
+    # isLifeCycleMethod , all trading logic here
     def on_trading_iteration(self):
+        last_price,cash,quantity = self.position_sizing()
         if self.last_trade is None:
             order = self.create_order(
                 asset = self.symbol,
-                quantity= 10,
+                quantity= quantity,
                 side="buy",
-                type="market"
+                type="bracket",
+                stop_loss_price=last_price*.95,
+                take_profit_price=last_price*1.20
             )
             self.submit_order(order=order)
             self.last_trade = "buy"
@@ -44,8 +55,8 @@ backtesting_end_date = datetime(2023, 12, 31)
 
 broker = Alpaca(config=ALPACA_CREDS)
 asset = Asset(symbol='SPY',asset_type='stock')
-strategy = MLTrader(name='mlstrat', broker=broker, parameters={"symbol":asset.symbol})
+strategy = MLTrader(name='mlstrat', broker=broker, parameters={"symbol":asset.symbol,"cash_at_risk":0.5})
 strategy.backtest(YahooDataBacktesting,
                   backtesting_start_date,
                   backtesting_end_date,
-                  parameters={"symbol":asset.symbol})
+                  parameters={"symbol":asset.symbol,"cash_at_risk":0.5})
